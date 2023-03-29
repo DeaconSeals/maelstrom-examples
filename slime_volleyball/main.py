19908bc948475bdfe67329bcd175b0f90a3e8ac3
roberte777 <robewilkes@yahoo.com>
2023-03-29 00:12:08 -0500

    basic done

--- slime_volleyball/main.py
+++ slime_volleyball/main.py
@@ -1,12 +1,14 @@
+"""driver"""
+from time import sleep
 import gym
+import matplotlib.pyplot as plt
 from maelstrom.individual import GeneticProgrammingIndividual
 from numpy import random
-from slimevolleygym import SlimeVolleyEnv, gym
+from slimevolleygym import gym
 from snake_eyes import read_config
 from maelstrom.population import GeneticProgrammingPopulation, statistics
 from maelstrom import Maelstrom
 from slime_volleyball.primitives import *
-from time import sleep
 
 
 def main():
@@ -19,6 +21,9 @@ def main():
     maelstrom = maelstrom.run()
     print(maelstrom.log)
     env.close()
+    # plot the results
+    for run in maelstrom.log.values():
+        plot_data(run, "Maelstrom results", "Generation", "Fitness")
 
 
 def fitness(
@@ -80,8 +85,6 @@ def fitness(
     best_right = max(right_slime.population, key=lambda x: x.fitness)
     best_left = max(left_slime.population, key=lambda x: x.fitness)
 
-    # print(best_right.genotype.print_tree())
-    # print(best_left.genotype.print_tree())
     results = play_match(env, best_right, best_left, render=True)
     env.reset()
     print("Contender 1: ", best_right.fitness)
@@ -116,11 +119,14 @@ def evaluate_matches(
     for match in matches:
         right_opp = right_pop[match[0]]
         left_opp = left_pop[match[1]]
-        right_reward, left_reward = play_match(env, right_opp, left_opp)
+        # right_reward, left_reward = play_match(env, right_opp, left_opp)
+        reward = play_match(env, right_opp, left_opp)
         env.reset()
 
-        right_opp.trials.append(right_reward)
-        left_opp.trials.append(left_reward)
+        # right_opp.trials.append(right_reward)
+        # left_opp.trials.append(left_reward)
+        right_opp.trials.append(reward)
+        left_opp.trials.append(-reward)
     for individual in right_pop:
         individual.fitness = statistics.mean(individual.trials)
     for individual in left_pop:
@@ -133,13 +139,14 @@ def play_match(
     left_opp: GeneticProgrammingIndividual,
     render=False,
 ) -> int:
-    right_reward = 0
-    left_reward = 0
+    # right_reward = 0
+    # left_reward = 0
+    total_reward = 0
     right_obs = env.reset()
     left_obs = right_obs
     if right_opp.genotype is None or left_opp.genotype is None:
         raise ValueError("Genotype must not be None")
-    for rounds_kept_up in range(10000):
+    for rounds_kept_up in range(1000000):
         right_q_values = []
         left_q_values = []
         for action in range(3):
@@ -159,49 +166,66 @@ def play_match(
 
         right_obs, reward, done, info = env.step(right_action, left_action)
         left_obs = info["otherObs"]
-        right_reward += 1
-        left_reward += 1
-        right_reward += rounds_kept_up * 0.01
-        left_reward += rounds_kept_up * 0.01
-        if reward == 1:
-            right_reward += 40
-            left_reward -= 40
-        elif reward == -1:
-            right_reward -= 40
-            left_reward += 40
+        # right_reward += 1
+        # left_reward += 1
+        # if reward == 1:
+        # right_reward += 20
+        # left_reward -= 20
+        # elif reward == -1:
+        #     right_reward -= 20
+        #     left_reward += 20
+        total_reward += reward
 
         if render:
             sleep(0.02)
             env.render()
         if done:
-            if reward == 1:
-                right_reward += 50
-                left_reward -= 50
-            elif reward == -1:
-                right_reward -= 50
-                left_reward += 50
+            # if reward == 1:
+            #     right_reward += 50
+            #     left_reward -= 50
+            # elif reward == -1:
+            #     right_reward -= 50
+            #     left_reward += 50
             break
-    return right_reward, left_reward
+    # return right_reward, left_reward
+    return total_reward
 
 
 def gather_data(
-    pop1: GeneticProgrammingPopulation, pop2: GeneticProgrammingPopulation, evals=None
+    right_pop: GeneticProgrammingPopulation,
+    left_pop: GeneticProgrammingPopulation,
+    evals=None,
 ) -> dict:
     """
     Gather data from the population
     """
-    if pop1.population is None or pop2.population is None:
+    if right_pop.population is None or left_pop.population is None:
         raise ValueError("Pops must not be None")
     log = {}
-    pop1_fitness = [i.fitness for i in pop1.population]
-    log["avg_pop1_fitness"] = sum(pop1_fitness) / len(pop1_fitness)
-    log["max_pop1_fitness"] = max(pop1_fitness)
-    pop2_fitness = [i.fitness for i in pop2.population]
-    log["avg_pop2_fitness"] = sum(pop2_fitness) / len(pop2_fitness)
-    log["max_pop2_fitness"] = max(pop2_fitness)
+    right_fitness = [i.fitness for i in right_pop.population]
+    log["avg_right_fitness"] = sum(right_fitness) / len(right_fitness)
+    log["max_right_fitness"] = max(right_fitness)
+    left_fitness = [i.fitness for i in left_pop.population]
+    log["avg_left_fitness"] = sum(left_fitness) / len(left_fitness)
+    log["max_left_fitness"] = max(left_fitness)
     if evals:
         log["evals"] = evals
     return log
+
+
+def plot_data(data: list[dict], title: str, y_label: str, x_label: str):
+    """
+    Plots the data from the log
+    """
+    fig, ax = plt.subplots()
+    ax.plot(
+        [i for i, _ in enumerate(data["avg_right_fitness"])], data["avg_right_fitness"]
+    )
+    ax.plot(
+        [i for i, _ in enumerate(data["avg_left_fitness"])], data["avg_left_fitness"]
+    )
+    ax.set(xlabel=x_label, ylabel=y_label, title=title)
+    plt.show()
 
 
 if __name__ == "__main__":
